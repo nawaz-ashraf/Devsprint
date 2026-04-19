3b5bf2ac359361d80ee8d20067ee1f85d4e5e1ef
Nawaz Ashraf <sr.nawaz446@example.com>
2026-04-19 15:33:42 +0530

    ci: add assetPrefix and trailingSlash for robust GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,21 @@
 import type { NextConfig } from "next";
 
+const isProd = true;
+
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
   output: "export",
-  basePath: "/Devsprint",
+
+  basePath: isProd ? "/Devsprint" : "",
+  assetPrefix: isProd ? "/Devsprint/" : "",
+
   images: {
     unoptimized: true,
     formats: ["image/avif", "image/webp"],
   },
+
+  trailingSlash: true,
+  reactStrictMode: true,
+
   experimental: {
     optimizePackageImports: ["@react-three/fiber", "@react-three/drei", "gsap"],
   },
